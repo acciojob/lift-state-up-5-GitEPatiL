ca71e2fb724bf9637cee8d7edcb6b9c84ae2b5c5
Aviraj Gite <giteaviraj155@gmail.com>
2026-04-15 11:21:14 +0530

    Solved state lift up problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Login from './Login'
 
 const App = () => {
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h2>Lift State Up Example</h2>
+
+        {
+          isLoggedIn ? (<h3>You are logged in</h3>) : (<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>)
+        }
+        
     </div>
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Login({setIsLoggedIn}){
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        setIsLoggedIn(true)
+    }
+    return(
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="text" name="username" />
+        <label>password</label>
+        <input type="text" name="password" />
+        <button type="submit">submit</button>
+      </form>
+    );
+}
+
+export default Login;
